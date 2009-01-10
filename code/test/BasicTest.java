f66c475fed127c1c5f451a5416509a675be69fa1
Frederic Jean <fred@fredjean.net>
2009-01-10 11:43:43 -0700

    Making progress with the presentation

---
+++ code/test/BasicTest.java
@@ -0,0 +1,13 @@
+package test;
+
+import org.junit.Test; 
+import static org.junit.Assert.assertEquals; 
+
+public class BasicTest { 
+   
+   @Test 
+   public void helloWorld() { 
+      assertEquals("The answer should be 42", 42, 40 + 2); 
+   } 
+   
+}
